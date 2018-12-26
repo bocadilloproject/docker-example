0d88d52db2766c6ff0568d64bea73cb99ddf9e26
florimondmanca <florimond.manca@gmail.com>
2018-12-26 01:06:03 +0100

    add api.py

---
+++ api.py
@@ -0,0 +1,12 @@
+from bocadillo import API
+
+api = API()
+
+
+@api.route("/")
+async def index(req, res):
+    res.html = await api.template("index.html")
+
+
+if __name__ == '__main__':
+    api.run()
